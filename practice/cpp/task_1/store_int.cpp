097a371cecf5538ecb5652cf36b258968d274f38
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-09 21:11:24 +0100

    task_1 - store integers with predefined data size + display them on the console, TO BE ADDED - resize data dynamically by adding/removing elements

---
+++ practice/cpp/task_1/store_int.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <array>
+using namespace std;
+
+int fixed_storage[4];
+int populate_fixed_storage();
+int display_fixed_storage();
+
+int main()
+{
+    display_fixed_storage();
+    return 0;
+}
+
+int populate_fixed_storage()
+{
+    cout << "Insert numbers into an array: " << endl;
+    for (int i = 0; i < sizeof(fixed_storage) / sizeof(fixed_storage[0]); i++)
+    {
+        cin >> fixed_storage[i];
+    }
+    return 0;
+}
+
+int display_fixed_storage()
+{
+    populate_fixed_storage();
+    cout << "Your array contains following elements:" << endl;
+    for (int i = 0; i < sizeof(fixed_storage) / sizeof(fixed_storage[0]); i++)
+    {
+        cout << fixed_storage[i] << endl;
+    }
+    return 0;
+}
